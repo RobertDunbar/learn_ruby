d278d363acf9c799109ad82bc042a48934dfff66
Robert Dunbar <robert_dunbar@me.com>
2019-06-06 21:40:14 +0100

    simon says done

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,48 @@
 #write your code here
+def echo phrase
+    phrase
+end
+
+def shout phrase
+    phrase.upcase
+end
+
+def repeat phrase, num = 2
+    output = ""
+    num.times do |count|
+        output += phrase
+        if count < num -1
+            output += " "
+        end
+    end
+    output
+end
+
+def start_of_word phrase, numLetters
+    phrase[0, numLetters]
+end
+
+def first_word phrase
+    output = phrase.split(" ")
+    output[0]
+end
+
+def titleize phrase
+    splitStr = phrase.split(" ")
+    output = ""
+    counter = 0
+    splitStr.each do |word|
+        if (word == "the" || word == "and" || word == "over") && counter == 0
+            output += word.capitalize
+        elsif (word != "the" && word != "and" && word != "over")
+            output += word.capitalize
+        else
+            output += word
+        end
+        counter += 1
+        if counter < splitStr.length
+            output += " "
+        end
+    end
+    output
+end
