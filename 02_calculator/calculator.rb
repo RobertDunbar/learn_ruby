99557301fc3f2bb2bf8f2242ace358cfb4ec6193
Robert Dunbar <robert_dunbar@me.com>
2019-06-06 18:29:15 +0100

    calculator finished

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,42 @@
 #write your code here
+def add num1, num2
+    num1 + num2
+end
+
+def subtract num1, num2
+    num1 - num2
+end
+
+def sum array
+    result = 0
+    array.each do |num|
+        result += num
+    end
+    result
+end
+
+def multiply arrayNum
+    result = 1
+    arrayNum.each do |num|
+        result *= num
+    end
+
+    result
+end
+
+def power num, pow
+    num ** pow
+end
+
+def factorial num
+    result = 1
+    if (num == 0 || num == 1)
+        return result
+    end
+
+    num.times do
+        result *= num
+        num -= 1
+    end
+    result
+end
